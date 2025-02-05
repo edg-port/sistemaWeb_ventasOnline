f4d9cea7b95f479012289099d8c644f4fc00ca70
edg-port <edgaramm112@gmail.com>
2025-02-05 13:41:03 -0600

    first commit

---
+++ sistemaWeb_ventasOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sistemaWeb_ventasOnline.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
